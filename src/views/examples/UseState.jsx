6f8f29c182c5ba9240f9ba7d7318e0086c94991b
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2021-01-18 15:31:50 -0300

    Adicionando exercicios

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -1,36 +1,36 @@
-import React, { useState } from 'react'
-import PageTitle from '../../components/layout/PageTitle'
-import SectionTitle from '../../components/layout/SectionTitle'
+import React, { useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
 const UseState = (props) => {
-    const [count, setCount] = useState(0)
-    const [name, setName] = useState("")
-    
-    return (
-        <div className="UseState">
-            <PageTitle
-                title="Hook UseState"
-                subtitle="Estado em componentes funcionais!" />
-            
-            <SectionTitle title="Exercício #01" />
-            <div className="center">
-                <span className="text">{count}</span>
-                <div>
-                    <button className="btn"
-                        onClick={() => setCount(count - 1)}>-1</button>
-                    <button className="btn"
-                        onClick={() => setCount(count + 1)}>+1</button>
-                    <button className="btn"
-                        onClick={() => setCount(currect => currect + 1000)}>+1000</button>
-                </div>
-            </div>
+  // Descructuring= Quando vc cria 2 expressões vc esta lendo o conteúdo do array
+  const [count, setCount] = useState(0);
+  const [name,setName]=useState("Inicial")
 
-            <SectionTitle title="Exercício #02" />
-            <input type="text" className="input"
-                value={name} onChange={e => setName(e.target.value)} />
-            <span className="text">{name}</span>
+  return (
+    <div className="UseState">
+      <PageTitle
+        title="Hook UseState"
+        subtitle="Estado em componentes funcionais !"/>
+      <SectionTitle title="Exercício #01" />
+      <div className="center">
+        <span className="text">{count}</span>
+        <div>
+          <button className="btn"
+          onClick={()=>setCount(count+1)}>+1</button>
+          {/* dentro do botão vai chamar o setcount pegando o valor atual (count) e adicionando*/}  
+          <button className="btn"
+          onClick={()=>setCount(count-1)}>-1</button>
+          <button className="btn"
+          onClick={()=>setCount(current=>current+1000)}>1000</button>
         </div>
-    )
-}
-
-export default UseState
+      </div>
+      <SectionTitle title="Exercício #02" />
+      <input type="text" className="input"
+      
+         value={name} onChange={e=> setName(e.target.value)}/>
+         <span className="text">{name}</span>
+    </div>
+  );
+};
+export default UseState;
